4d0a60b198a0baf80217bd98c4c17dccfd529604
Jose J. Cruz Torres <jjzcru@gmail.com>
2020-01-04 13:31:37 -0500

    Add support for json file

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
+const fs = require('fs');
 const open = require('open');
 const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const YAML = require('yamljs');
 
-const port = 3000
- 
+const port = getPort();
+const swaggerFile = getTargetFile();
+
+const swaggerDocument = require(swaggerFile); 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, async () => {
     try {
-        console.log(`Swagger UI Server listening on port ${port}!`)
-        await open(`http://localhost:${port}`);
+        getTargetFile() 
+        console.log(`Swagger UI Server listening on port ${port}`)
+        // await open(`http://localhost:${port}`);
     } catch(e) {
         console.error(e);
     }
-})
+});
+
+function getPort() {
+    if(fs.existsSync('./package.json')) {
+        const pkg = require('./package.json');
+        if(!!pkg.swagger && !!pkg.swagger.port) {
+            return parseInt(pkg.swagger.port);
+        }
+    }
+
+    return 5466;
+}
+
+function getTargetFile() {
+    let filePath = getFileFromPkg();
+    if(!!filePath && fs.existsSync(filePath)) {
+        return filePath;
+    }
+
+    throw new Error(`Swagger configuration file '${filePath}' do not exist`)
+}
+
+function getFileFromPkg() {
+    if(fs.existsSync('./package.json')) {
+        const pkg = require('./package.json');
+        if(!!pkg.swagger && !!pkg.swagger.file) {
+            return pkg.swagger.file;
+        }
+    }
+    return './swagger.json';
+}
